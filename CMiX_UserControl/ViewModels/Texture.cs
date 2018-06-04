f3436e1cedd95a785d64db4da0d8f3b8f7a48029
Blake Burgess <trinitek@live.com>
2018-06-04 05:03:13 -0400

    Add ctor to Texture

--- CMiX_UserControl/ViewModels/Texture.cs
+++ CMiX_UserControl/ViewModels/Texture.cs
@@ -1,56 +1,83 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 
 namespace CMiX.ViewModels
 {
     public class Texture : ViewModel
     {
-        List<string> _SelectedTexturePath = new List<string>();
-        public List<string> SelectedTexturePath
+        public Texture()
+            : this(
+                  brightness: 0.0,
+                  contrast: 0.0,
+                  saturation: 0.0,
+                  keying: 0.0,
+                  invert: 0.0,
+                  invertMode: default)
+        { }
+
+        public Texture(
+            double brightness,
+            double contrast,
+            double saturation,
+            double keying,
+            double invert,
+            TextureInvertMode invertMode)
         {
-            get => _SelectedTexturePath;
-            set => SetAndNotify(ref _SelectedTexturePath, value);
+            AssertNotNegative(() => brightness);
+            AssertNotNegative(() => contrast);
+            AssertNotNegative(() => saturation);
+            AssertNotNegative(() => keying);
+            AssertNotNegative(() => invert);
+
+            Brightness = brightness;
+            Contrast = contrast;
+            Saturation = saturation;
+            Keying = keying;
+            Invert = invert;
+            InvertMode = invertMode;
         }
 
-        double _TextureBrightness = 0.0;
-        public double TextureBrightness
+        public ObservableCollection<string> SelectedTexturePaths { get; }
+
+        private double _brightness;
+        public double Brightness
         {
-            get => _TextureBrightness;
-            set => SetAndNotify(ref _TextureBrightness, value);
+            get => _brightness;
+            set => SetAndNotify(ref _brightness, CoerceNotNegative(value));
         }
 
-        double _TextureContrast = 0.0;
-        public double TextureContrast
+        private double _contrast;
+        public double Contrast
         {
-            get => _TextureContrast;
-            set => SetAndNotify(ref _TextureContrast, value);
+            get => _contrast;
+            set => SetAndNotify(ref _contrast, CoerceNotNegative(value));
         }
 
-        double _TextureSaturation = 0.0;
-        public double TextureSaturation
+        private double _saturation;
+        public double Saturation
         {
-            get => _TextureSaturation;
-            set => SetAndNotify(ref _TextureSaturation, value);
+            get => _saturation;
+            set => SetAndNotify(ref _saturation, CoerceNotNegative(value));
         }
 
-        double _TextureKeying = 0.0;
-        public double TextureKeying
+        private double _keying;
+        public double Keying
         {
-            get => _TextureKeying;
-            set => SetAndNotify(ref _TextureKeying, value);
+            get => _keying;
+            set => SetAndNotify(ref _keying, CoerceNotNegative(value));
         }
 
-        double _TextureInvert = 0.0;
-        public double TextureInvert
+        private double _invert;
+        public double Invert
         {
-            get => _TextureInvert;
-            set => SetAndNotify(ref _TextureInvert, value);
+            get => _invert;
+            set => SetAndNotify(ref _invert, CoerceNotNegative(value));
         }
 
-        TextureInvertMode _InvertMode;
+        private TextureInvertMode _invertMode;
         public TextureInvertMode InvertMode
         {
-            get => _InvertMode;
-            set => SetAndNotify(ref _InvertMode, value);
+            get => _invertMode;
+            set => SetAndNotify(ref _invertMode, value);
         }
     }
 }
--- CMiX_UserControl/ViewModels/ViewModel.cs
+++ CMiX_UserControl/ViewModels/ViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 
 namespace CMiX.ViewModels
@@ -16,6 +18,29 @@ namespace CMiX.ViewModels
         {
             backingField = newValue;
             NotifyPropertyChanged(propertyName);
+        }
+
+        public static void AssertNotNegative(double value, string parameterName)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Value must not be negative.", parameterName);
+            }
+        }
+
+        public static void AssertNotNegative(Expression<Func<double>> parameterExpression)
+        {
+            double value = parameterExpression.Compile().Invoke();
+
+            if (value < 0)
+            {
+                throw new ArgumentException("Value must not be negative.", ((MemberExpression)parameterExpression.Body).Member.Name);
+            }
+        }
+
+        public static double CoerceNotNegative(double value)
+        {
+            return value < 0 ? 0 : value;
         }
     }
 }
