3b22cd00e23a94eb22dc4bca36ee8865aa9bdfda
cloneproduction <clone@cloneproduction.net>
2019-07-16 16:59:09 +0800

    pouet

---
+++ CMiXPlayer/Jobs/JobSendComposition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using CMiX.MVVM.Models;
+using CMiX.MVVM.ViewModels;
+using System.IO;
+using Ceras;
+
+namespace CMiXPlayer.Jobs
+{
+    class JobSendComposition : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var dataMap = context.MergedJobDataMap;
+            FileSelector compositionmodel = (FileSelector)dataMap["CompoSelector"];
+            OSCMessenger oscmessenger = (OSCMessenger)dataMap["OSCMessenger"];
+            CerasSerializer serializer = (CerasSerializer)dataMap["Serializer"];
+
+            SendComposition(oscmessenger, compositionmodel, serializer);
+
+            await Console.Out.WriteLineAsync("Composition Sent");
+        }
+
+        private void SendComposition(OSCMessenger oscmessenger, FileSelector composelector, CerasSerializer serializer)
+        {
+            var filename = composelector.SelectedFileNameItem.FileName;
+            byte[] data = File.ReadAllBytes(filename);
+            CompositionModel compositionmodel = serializer.Deserialize<CompositionModel>(data);
+            oscmessenger.SendMessage("/CompositionReloaded", true);
+            oscmessenger.QueueObject(compositionmodel);
+            oscmessenger.SendQueue();
+        }
+    }
+}
--- CMiXPlayer/ViewModels/Project.cs
+++ CMiXPlayer/ViewModels/Project.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Windows.Input;
 using Ceras;
 using CMiX.MVVM.ViewModels;
 using Memento;
+using Quartz;
+using Quartz.Impl;
 
 namespace CMiXPlayer.ViewModels
 {
@@ -21,6 +24,7 @@ namespace CMiXPlayer.ViewModels
             DeleteClientCommand = new RelayCommand(p => DeleteClient(p));
             SendAllCommand = new RelayCommand(p => SendAllClient());
             ResetAllClientCommand = new RelayCommand(p => ResetAllClient());
+            MakeJobCommand = new RelayCommand(p => MakeJob());
         }
         #endregion
 
@@ -29,11 +33,45 @@ namespace CMiXPlayer.ViewModels
         public ICommand DeleteClientCommand { get; }
         public ICommand SendAllCommand { get; }
         public ICommand ResetAllClientCommand { get; }
+        public ICommand MakeJobCommand { get; }
 
         public CerasSerializer Serializer { get; set; }
         public ObservableCollection<Client> Clients { get; set; }
         public FileSelector CompoSelector { get; set; }
         #endregion
+
+        public async void MakeJob()
+        {
+            // construct a scheduler factory
+            NameValueCollection props = new NameValueCollection{{ "quartz.serializer.type", "binary" }};
+            StdSchedulerFactory factory = new StdSchedulerFactory(props);
+
+            // get a scheduler
+            IScheduler sched = await factory.GetScheduler();
+            await sched.Start();
+
+            JobDataMap jobdatamap = new JobDataMap();
+            jobdatamap.Add("CompoSelector", Clients[0].CompoSelector);
+            jobdatamap.Add("OSCMessenger", Clients[0].OSCMessenger);
+            jobdatamap.Add("Serializer", Clients[0].Serializer);
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<Jobs.JobSendComposition>()
+                .SetJobData(jobdatamap)
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(10)
+                    .WithRepeatCount(2))
+            .Build();
+
+            await sched.ScheduleJob(job, trigger);
+        }
 
         #region METHODS
         private void AddClient()
