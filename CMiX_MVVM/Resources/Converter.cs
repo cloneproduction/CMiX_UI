bb1e412647989f9fad7917ed1f0f42a172690c1e
cloneproduction <clone@cloneproduction.net>
2019-09-05 21:57:20 +0800

    Fixing textbox on colorpicker

--- CMiX_MVVM/Resources/Converter.cs
+++ CMiX_MVVM/Resources/Converter.cs
@@ -123,7 +123,62 @@ namespace CMiX.MVVM.Resources
 
 
 
+    public class ByteToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
 
+            byte b = (byte)value;
+
+            return b.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var st = (string)value;
+
+            if(!string.IsNullOrEmpty(st))
+            {
+                int b = System.Convert.ToInt32(value);
+                if(b < 0)
+                    return System.Convert.ToByte(0);
+                else if(b > 255)
+                    return System.Convert.ToByte(255);
+                else
+                    return System.Convert.ToByte(value);
+            }
+            else
+                return System.Convert.ToByte(0);
+        }
+    }
+
+    public class DoubleToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            double b = (double)value;
+
+            return b.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var st = (string)value;
+
+            if (!string.IsNullOrEmpty(st))
+            {
+                double b = System.Convert.ToDouble(value);
+                if (b < 0)
+                    b = 0.0;
+                else if (b > 255)
+                    b = 255;
+                return b;
+            }
+            else
+                return (double)value;
+        }
+    }
 
     public class ByteToNormalizedDoubleConverter : IValueConverter
     {
