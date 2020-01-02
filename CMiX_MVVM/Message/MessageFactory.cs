c16b519a9f6101c11bc5998c8c226e9175a80665
cloneproduction <clone@cloneproduction.net>
2020-01-02 19:22:38 +0800

    added MessageFactory class

---
+++ CMiX_MVVM/Message/MessageFactory.cs
@@ -0,0 +1,32 @@
+﻿using CMiX.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMiX.MVVM.Message
+{
+    public class MessageFactory
+    {
+        public MessageFactory()
+        {
+
+        }
+
+        int servernameid = 0;
+
+        public Server CreateServer()
+        {
+            Server server = new Server($"Server({servernameid.ToString()})", "127.0.0.1", 1111 + servernameid, $"/Device{servernameid}");
+            server.Start();
+            servernameid++;
+            return server;
+        }
+
+        //public Client CreateClient()
+        //{
+
+        //}
+    }
+}
