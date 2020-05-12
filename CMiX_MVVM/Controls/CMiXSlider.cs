e9f17c4dce8c36b9aaf7e2354b34512a826dd984
cloneproduction <clone@cloneproduction.net>
2020-05-12 13:14:28 +0800

    pouet

---
+++ CMiX_MVVM/Controls/CMiXSlider.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CMiX.MVVM.Controls
+{
+    public class CMiXSlider : Slider
+    {
+        public CMiXSlider()
+        {
+            OnApplyTemplate();
+        }
+
+        EditableValue EditableValue { get; set; }
+
+        public override void OnApplyTemplate()
+        {
+            if(EditableValue != null)
+                EditableValue = GetTemplateChild("editableValue") as EditableValue;
+
+            base.OnApplyTemplate();
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            if (_lastPoint != null)
+            {
+                var currentPoint = GetMousePosition();
+                var offset = currentPoint - _lastPoint.Value;
+
+                newValue = this.Value + offset.X * ((Math.Abs(this.Minimum) + Math.Abs(this.Maximum)) / ActualWidth);
+
+                if (newValue >= this.Maximum)
+                    newValue = this.Maximum;
+                else if (newValue <= this.Minimum)
+                    newValue = this.Minimum;
+
+                this.Value = newValue;
+                _lastPoint = GetMousePosition();
+            }
+            base.OnPreviewMouseMove(e);
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            var mouseUpPos = e.GetPosition(this);
+
+            if (_mouseDownPos != null)
+            {
+                Point pointToScreen;
+
+                if (mouseUpPos.X > this.ActualWidth)
+                    pointToScreen = this.PointToScreen(new Point() { X = ActualWidth, Y = ActualHeight / 2 });
+                else if (mouseUpPos.X < 0)
+                    pointToScreen = this.PointToScreen(new Point() { X = 0, Y = ActualHeight / 2 });
+                else
+                    pointToScreen = this.PointToScreen(new Point() { X = mouseUpPos.X, Y = ActualHeight / 2 });
+
+                SetCursorPos(Convert.ToInt32(pointToScreen.X), Convert.ToInt32(pointToScreen.Y));
+            }
+
+            if (_mouseDownPos == mouseUpPos)
+            {
+                //EditableValue.IsEditing = true;
+                //Console.WriteLine("EditableValue.IsEditing " + EditableValue.IsEditing);
+            }
+
+            this.ReleaseMouseCapture();
+            _lastPoint = null;
+            base.OnPreviewMouseUp(e);
+        }
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            _lastPoint = GetMousePosition();
+            _mouseDownPos = e.GetPosition(this);
+            this.CaptureMouse();
+            base.OnPreviewMouseDown(e);
+        }
+
+
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        private Point? _mouseDownPos;
+        private Point? _lastPoint;
+        private double newValue;
+
+        public static readonly DependencyProperty CaptionProperty =
+        DependencyProperty.Register("Caption", typeof(string), typeof(Slider), new PropertyMetadata(""));
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+    }
+}
