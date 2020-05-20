f1321ba711f60f17fcb4c1292565c5318bc26bb0
cloneproduction <clone@cloneproduction.net>
2020-05-20 17:54:50 +0800

    pouet

--- CMiX_UserControl/Behaviors/ContextMenuMouseDownBehavior.cs
+++ CMiX_UserControl/Behaviors/ContextMenuMouseDownBehavior.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Interactivity;
@@ -22,7 +17,28 @@ namespace CMiX.Behaviors
 
         private void OnWindowLoaded(object sender, RoutedEventArgs e)
         {
-            this.AssociatedObject.MouseRightButtonDown += AssociatedObject_MouseRightButtonDown; ;
+            this.AssociatedObject.MouseRightButtonDown += AssociatedObject_MouseRightButtonDown;
+            this.AssociatedObject.MouseRightButtonUp += AssociatedObject_MouseRightButtonUp;
+        }
+
+        private void AssociatedObject_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is Control fe && fe.ContextMenu != null)
+            {
+                if (fe.ContextMenu != null)
+                {
+                    // If we use binding in our context menu, then it's DataContext won't be set when we show the menu on left click. It
+                    // seems setting DataContext for ContextMenu is hardcoded in WPF when user right clicks on a control? So we have to set
+                    // up ContextMenu.DataContext manually here.
+                    if (fe.ContextMenu?.DataContext == null)
+                    {
+                        fe.ContextMenu?.SetBinding(FrameworkElement.DataContextProperty, new Binding { Source = fe.DataContext });
+                    }
+
+                    fe.ContextMenu.IsOpen = false;
+                    e.Handled = true;
+                }
+            }
         }
 
         private void AssociatedObject_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -46,26 +62,8 @@ namespace CMiX.Behaviors
 
         private void OnWindowUnloaded(object sender, RoutedEventArgs e)
         {
-            this.AssociatedObject.MouseRightButtonDown -= AssociatedObject_MouseRightButtonDown; // Cannot override OnDetached(), as this is not called on Dispose. Known issue in WPF.
+            this.AssociatedObject.MouseRightButtonDown -= AssociatedObject_MouseRightButtonDown;
+            this.AssociatedObject.MouseLeftButtonDown -= AssociatedObject_MouseRightButtonUp;// Cannot override OnDetached(), as this is not called on Dispose. Known issue in WPF.
         }
-
-        //private static void OnMouseLeftButtonUp(object sender, RoutedEventArgs e)
-        //{
-        //    if (sender is ButtonBase fe && fe.ContextMenu != null)
-        //    {
-        //        if (fe.ContextMenu != null)
-        //        {
-        //            // If we use binding in our context menu, then it's DataContext won't be set when we show the menu on left click. It
-        //            // seems setting DataContext for ContextMenu is hardcoded in WPF when user right clicks on a control? So we have to set
-        //            // up ContextMenu.DataContext manually here.
-        //            if (fe.ContextMenu?.DataContext == null)
-        //            {
-        //                fe.ContextMenu?.SetBinding(FrameworkElement.DataContextProperty, new Binding { Source = fe.DataContext });
-        //            }
-
-        //            fe.ContextMenu.IsOpen = true;
-        //        }
-        //    }
-        //}
     }
 }
