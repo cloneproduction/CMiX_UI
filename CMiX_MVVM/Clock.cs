8dd197e11c2080a205b5b166934b7477c710d539
cloneproduction <clone@cloneproduction.net>
2020-06-09 00:07:32 +0800

    pouet

---
+++ CMiX_MVVM/Clock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMiX.MVVM
+{
+    public class Clock
+    {
+        private delegate void TimerEventHandler(int id, int msg, IntPtr user, int dw1, int dw2);
+
+        private const int TIME_PERIODIC = 1;
+        private const int EVENT_TYPE = TIME_PERIODIC;
+
+        [DllImport("winmm.dll")]
+        private static extern int timeSetEvent(int delay, int resolution, TimerEventHandler handler, IntPtr user, int eventType);
+        [DllImport("winmm.dll")]
+        private static extern int timeKillEvent(int id);
+        [DllImport("winmm.dll")]
+        private static extern int timeBeginPeriod(int msec);
+        [DllImport("winmm.dll")]
+        private static extern int timeEndPeriod(int msec);
+
+        public int _timerId;
+        private TimerEventHandler _handler = delegate { };
+
+        public event EventHandler OnTick;
+
+        public int Interval { get; set; }
+
+        public Clock()
+        {
+            Interval = 1000;
+            //OnTick = tick;
+            _handler = new TimerEventHandler(timerElapsed);
+        }
+
+        public void Stop()
+        {
+            int res = timeKillEvent(_timerId);
+            timeEndPeriod(1);
+            _timerId = 0;
+        }
+
+        public void Start()
+        {
+            timeBeginPeriod(1);
+            _timerId = timeSetEvent(Interval, 0, _handler, IntPtr.Zero, EVENT_TYPE);
+        }
+        private void timerElapsed(int id, int msg, IntPtr user, int dw1, int dw2)
+        {
+            OnTick(this, new EventArgs());
+        }
+    }
+}
--- CMiX_MVVM/ViewModels/Beat/MasterBeat.cs
+++ CMiX_MVVM/ViewModels/Beat/MasterBeat.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Timers;
+using System.Threading;
 using System.Windows.Input;
+using System.Windows.Threading;
+
 
 namespace CMiX.MVVM.ViewModels
 {
@@ -11,26 +13,28 @@ namespace CMiX.MVVM.ViewModels
     {
         public MasterBeat(double period, double multiplier)
         {
-            Timer = new Timer();
-            Timer.Elapsed += Timer_Elapsed;
-            //Timer.Interval = 1000;
-            Timer.Start();
+            Clock = new Clock();
+            Clock.OnTick += Clock_OnTick;
             Period = period;
+            BeatTick = 0;
+
             Multiplier = multiplier;
             ResyncCommand = new RelayCommand(p => Resync());
             TapCommand = new RelayCommand(p => Tap());
             tapPeriods = new List<double>();
             tapTime = new List<double>();
-
-            BeatTaped = false;
         }
 
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        private void Clock_OnTick(object sender, EventArgs e)
         {
-            if (BeatTaped == false)
-                BeatTaped = true;
-            else
-                BeatTaped = false;
+            BeatTick++;
+            if (BeatTick >= 4)
+                BeatTick = 0;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
         }
 
         public MasterBeat() : this (period: 0.0, multiplier: 1)
@@ -40,15 +44,17 @@ namespace CMiX.MVVM.ViewModels
 
         public ICommand ResyncCommand { get; }
         public ICommand TapCommand { get; }
-        public Timer Timer { get; set; }
+        public Clock Clock { get; set; }
+        public DispatcherTimer Timer { get; set; }
+        public Stopwatch Stopwatch { get; set; }
 
-        private bool _beatTaped;
-        public bool BeatTaped
+
+        private int _beatTick;
+        public int BeatTick
         {
-            get => _beatTaped;
-            set => SetAndNotify(ref _beatTaped, value);
+            get => _beatTick;
+            set => SetAndNotify(ref _beatTick, value);
         }
-
 
         private readonly List<double> tapPeriods;
         private readonly List<double> tapTime;
@@ -65,15 +71,26 @@ namespace CMiX.MVVM.ViewModels
                 OnPeriodChanged(Period);
                 Notify(nameof(BPM));
                 if (Period > 0)
-                    Timer.Interval = Period / 2;
-                //SendMessages(MessageAddress + nameof(Period), Period);
+                {
+                    Clock.Stop();
+                    Clock.Interval = Convert.ToInt32(Period);
+                    Clock.Start();
+                }
+                    
+                //    Timer.Interval = new TimeSpan(Convert.ToInt64(Period) * Convert.ToInt64(10000));
             }
         }
 
         #region METHODS
         private void Resync()
         {
-            //Sender.SendMessages(MessageAddress + nameof(Resync), CurrentTime + Period);
+            BeatTick = 0;
+            Clock.Stop();
+            Clock.Start();
+            //Timer.Stop();
+            //Timer.Interval = new TimeSpan(Convert.ToInt64(Period * 10000));
+            //Timer.Start();
+            
         }
 
         protected override void Multiply()
