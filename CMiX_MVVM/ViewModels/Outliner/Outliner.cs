1e85e926018cf20016cffb38aee9838e8d444de7
cloneproduction <clone@cloneproduction.net>
2020-10-24 13:13:45 +0800

    pouet

--- CMiX_MVVM/ViewModels/Outliner/Outliner.cs
+++ CMiX_MVVM/ViewModels/Outliner/Outliner.cs
@@ -108,10 +108,7 @@ namespace CMiX.MVVM.ViewModels
 
         public void CanRenameComponent()
         {
-            if (SelectedComponent is Project || SelectedComponent is Composition || SelectedComponent is Layer || SelectedComponent is Scene || SelectedComponent is Entity)
-                CanRename = true;
-            else
-                CanRename = false;
+            CanRename = true;
         }
 
         public void CanEditComponent()
@@ -130,27 +127,10 @@ namespace CMiX.MVVM.ViewModels
                 CanDelete = false;
         }
 
-        public int FindItemParentIndex(ObservableCollection<Component> components, Component componentToFind)
-        {
-            int index = -1;
-
-            if (components.Contains(componentToFind))
-            {
-                index = components.IndexOf(componentToFind);
-            }
-            else
-            {
-                foreach (var item in components)
-                {
-                    FindItemParentIndex(item.Components, componentToFind);
-                }
-            }
-            return index;
-        }
-
-
 
         
+
+
         private void DropFromLastItem(TreeViewItem target, TreeViewItem source)
         {
             if(target != null && source != null)
@@ -167,7 +147,9 @@ namespace CMiX.MVVM.ViewModels
                     Component targetGrandParentComponent = Utils.FindParent<TreeViewItem>(targetParent).DataContext as Component;
                     sourceParentComponent.Components.Remove(sourceComponent);
                     targetGrandParentComponent.Components.Add(sourceComponent);
-                    
+
+                    sourceParentComponent.OnSendChange(sourceParentComponent.GetModel(), sourceParentComponent.GetMessageAddress());
+                    targetGrandParentComponent.OnSendChange(targetGrandParentComponent.GetModel(), targetGrandParentComponent.GetMessageAddress());
                 }
                 else
                 {
@@ -180,8 +162,6 @@ namespace CMiX.MVVM.ViewModels
         {
             if (target != null && source != null)
             {
-
-                Console.WriteLine("DropOnSameTargetType");
                 TreeViewItem targetParent = Utils.FindParent<TreeViewItem>(target);
                 TreeViewItem sourceParent = Utils.FindParent<TreeViewItem>(source);
 
@@ -195,8 +175,8 @@ namespace CMiX.MVVM.ViewModels
                 sourceParentComponent.Components.Remove(sourceComponent);
                 targetParentComponent.Components.Insert(TargetIndex, sourceComponent);
 
-                //SourceComponentParent.OnSendChange(SourceComponentParent.GetModel(), SourceComponentParent.GetMessageAddress());
-                //TargetComponentParent.OnSendChange(TargetComponentParent.GetModel(), TargetComponentParent.GetMessageAddress());
+                sourceParentComponent.OnSendChange(SourceComponentParent.GetModel(), sourceParentComponent.GetMessageAddress());
+                targetParentComponent.OnSendChange(targetParentComponent.GetModel(), targetParentComponent.GetMessageAddress());
             }
         }
 
@@ -217,21 +197,23 @@ namespace CMiX.MVVM.ViewModels
 
                 if(targetParentComponent.GetType() == sourceComponent.GetType())
                 {
-                    Console.WriteLine("DropInSameParentType");
                     if (sourceParentComponent == targetGrandParentComponent)
                     {
                         if (SourceIndex > targetParentIndex)
                             targetParentIndex += 1;
 
                         targetGrandParentComponent.Components.Move(SourceIndex, targetParentIndex);
+                        targetGrandParentComponent.OnSendChange(targetGrandParentComponent.GetModel(), targetGrandParentComponent.GetMessageAddress());
                     }
                     else
                     {
                         sourceParentComponent.Components.Remove(sourceComponent);
                         targetGrandParentComponent.Components.Insert(targetParentIndex + 1, sourceComponent);
+
+                        sourceParentComponent.OnSendChange(sourceParentComponent.GetModel(), sourceParentComponent.GetMessageAddress());
+                        targetGrandParentComponent.OnSendChange(targetGrandParentComponent.GetModel(), targetGrandParentComponent.GetMessageAddress());
                     }
-                    //SourceComponentParent.OnSendChange(SourceComponentParent.GetModel(), SourceComponentParent.GetMessageAddress());
-                    //TargetComponentParent.OnSendChange(TargetComponentParent.GetModel(), TargetComponentParent.GetMessageAddress());
+
                 }
                 else
                 {
@@ -253,71 +235,13 @@ namespace CMiX.MVVM.ViewModels
 
                 sourceParentComponent.Components.Remove(sourceComponent);
                 targetComponent.Components.Insert(0, sourceComponent);
+
+                sourceParentComponent.OnSendChange(sourceParentComponent.GetModel(), sourceParentComponent.GetMessageAddress());
+                targetComponent.OnSendChange(targetComponent.GetModel(), targetComponent.GetMessageAddress());
             }
         }
-
-        public void Drop(IDropInfo dropInfo)
-        {
-            DoDropInParent.Invoke(VisualTarget, VisualSource);
-        }
-
 
         private Action<TreeViewItem, TreeViewItem> DoDropInParent { get; set; }
-
-
-        public void DragOver(IDropInfo dropInfo)
-        {
-            if (!dropInfo.Data.GetType().IsSubclassOf(typeof(Component)))
-                return;
-
-            GetInfo(dropInfo);
-
-            if (TargetComponent == null || TargetComponentParent == null)
-                return;
-
-            if (VisualTarget == VisualSource)
-                return;
-
-            if (ParentVisualTarget == null)
-                return;
-
-            if (SourceComponentParent != TargetComponent && SourceComponentParent.GetType() != typeof(Project)) // can't over it's own parent
-            {
-                if (SourceComponentParent.GetType() == TargetComponent.GetType())
-                {
-                    DoDropInParent = DropInDifferentParent;
-                    dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
-                    dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
-                }
-                else if (!InsertPositionTargetItemCenter )
-                {
-                    if (CheckTargetIsSameType(dropInfo))
-                    {
-                        Console.WriteLine("CheckTargetIsSameType");
-                        DoDropInParent = DropOnSameTargetType;
-                        dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
-                        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
-                    }
-
-                    else if (CheckParentIsSameType(VisualTarget, VisualSource, dropInfo.InsertPosition))
-                    {
-                        Console.WriteLine("CheckParentIsSameType");
-                        DoDropInParent = DropInSameParentType;
-                        dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
-                        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
-                    }
-                    else if (CheckItemIsLast(VisualTarget, VisualSource, dropInfo.InsertPosition))
-                    {
-                        Console.WriteLine("CheckItemIsLast");
-                        DoDropInParent = DropFromLastItem;
-                        dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
-                        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
-                    }
-                }
-            }
-        }
-
-
 
 
         private bool CheckTargetIsSameType(IDropInfo dropInfo)
@@ -424,20 +348,14 @@ namespace CMiX.MVVM.ViewModels
         private Component TargetComponent;
         private Component TargetComponentParent;
 
-        private ObservableCollection<Component> TargetCollection;
-        private ObservableCollection<Component> SourceCollection;
-
         private int TargetIndex = -1;
         private int SourceIndex = -1;
 
-        private Component DataObject;
-        private bool InsertPositionAfterTargetItem;
-        private bool InsertPositionBeforeTargetItem;
         private bool InsertPositionTargetItemCenter;
+
+
         private void GetInfo(IDropInfo dropInfo)
         {
-            DataObject = dropInfo.Data as Component;
-
             TargetIndex = dropInfo.InsertIndex;
             SourceIndex = dropInfo.DragInfo.SourceIndex;
 
@@ -447,21 +365,71 @@ namespace CMiX.MVVM.ViewModels
             SourceComponent = dropInfo.DragInfo.SourceItem as Component;
             TargetComponent = dropInfo.TargetItem as Component;
 
-
-            TargetCollection = dropInfo.TargetCollection as ObservableCollection<Component>;
-            SourceCollection = dropInfo.DragInfo.SourceCollection as ObservableCollection<Component>;
-
             ParentVisualSource = Utils.FindParent<TreeViewItem>(VisualSource);
             ParentVisualTarget = Utils.FindParent<TreeViewItem>(VisualTarget);
 
             InsertPositionTargetItemCenter = dropInfo.InsertPosition.HasFlag(RelativeInsertPosition.TargetItemCenter);
-            InsertPositionAfterTargetItem = dropInfo.InsertPosition == RelativeInsertPosition.AfterTargetItem && TargetIndex == SourceIndex;
-            InsertPositionBeforeTargetItem = dropInfo.InsertPosition == RelativeInsertPosition.BeforeTargetItem && TargetIndex == SourceIndex + 1;
 
             if (ParentVisualTarget != null)
             {
                 TargetComponentParent = ParentVisualTarget.DataContext as Component;
                 SourceComponentParent = ParentVisualSource.DataContext as Component;
+            }
+        }
+
+
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            DoDropInParent.Invoke(VisualTarget, VisualSource);
+        }
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+            if (!dropInfo.Data.GetType().IsSubclassOf(typeof(Component)))
+                return;
+
+            GetInfo(dropInfo);
+
+            if (TargetComponent == null || TargetComponentParent == null)
+                return;
+
+            if (VisualTarget == VisualSource)
+                return;
+
+            if (ParentVisualTarget == null)
+                return;
+
+            if (SourceComponentParent != TargetComponent && SourceComponentParent.GetType() != typeof(Project)) // can't over it's own parent
+            {
+                if (SourceComponentParent.GetType() == TargetComponent.GetType())
+                {
+                    DoDropInParent = DropInDifferentParent;
+                    dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
+                    dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+                }
+                else if (!InsertPositionTargetItemCenter)
+                {
+                    if (CheckTargetIsSameType(dropInfo))
+                    {
+                        DoDropInParent = DropOnSameTargetType;
+                        dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
+                        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+                    }
+
+                    else if (CheckParentIsSameType(VisualTarget, VisualSource, dropInfo.InsertPosition))
+                    {
+                        DoDropInParent = DropInSameParentType;
+                        dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
+                        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+                    }
+                    else if (CheckItemIsLast(VisualTarget, VisualSource, dropInfo.InsertPosition))
+                    {
+                        DoDropInParent = DropFromLastItem;
+                        dropInfo.Effects = DragDropEffects.Copy | DragDropEffects.Move;
+                        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+                    }
+                }
             }
         }
 
@@ -474,7 +442,7 @@ namespace CMiX.MVVM.ViewModels
         public bool CanStartDrag(IDragInfo dragInfo)
         {
             var dragged = dragInfo.SourceItem;
-            if (dragged is Entity || dragged is Layer || dragged is Scene)
+            if (dragged is Entity || dragged is Layer || dragged is Scene || dragged is Composition)
                 return true;
             else
                 return false;
