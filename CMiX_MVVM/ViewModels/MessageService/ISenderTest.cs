ff8606a6534ed4c54ef602749797553f7ca873e3
cloneproduction <clone@cloneproduction.net>
2021-02-16 00:09:01 +0800

    pouet

---
+++ CMiX_MVVM/ViewModels/MessageService/ISenderTest.cs
@@ -0,0 +1,10 @@
+﻿using CMiX.MVVM.Interfaces;
+
+namespace CMiX.MVVM.ViewModels
+{
+    public interface ISenderTest
+    {
+        void SetViewModel(IModel model);
+        IModel GetModel();
+    }
+}
